928fb4b54b3441a04bf77746da4a90ae30bbd672
Oll3Ljung <158461241+Oll3Ljung@users.noreply.github.com>
2024-02-14 16:50:05 +0100

    Quack 1.1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//Import the THREE.js library
+// Import the THREE.js library
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 // To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 // To allow for importing the .gltf file
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-//Create a Three.JS Scene
+// Create a Three.JS Scene
 const scene = new THREE.Scene();
-//create a new camera with positions and angles
+// Create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -15,28 +15,28 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-//Keep track of the mouse position, so we can make the eye move
+// Keep track of the mouse position, so we can make the eye move
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
 
-//Keep the 3D object on a global variable so we can access it later
+// Keep the 3D object on a global variable so we can access it later
 let object;
 
-//OrbitControls allow the camera to move around the scene
+// OrbitControls allow the camera to move around the scene
 let controls;
 
-//Set which object to render
+// Set which object to render
 let objToRender =
   "https://cdn.glitch.global/8b3f5487-a392-4a06-ac03-d2334be70d1c/Anka_1.3(stor).glb?v=1707477561872";
 
-//Instantiate a loader for the .gltf file
+// Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 
-//Load the file
+// Load the file
 loader.load(
   objToRender,
   function (gltf) {
-    //If the file is loaded, add it to the scene
+    // If the file is loaded, add it to the scene
     object = gltf.scene;
 
     // Calculate the bounding box of the object
@@ -53,83 +53,78 @@ loader.load(
     scene.add(object);
   },
   function (xhr) {
-    //While it is loading, log the progress
+    // While it is loading, log the progress
     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
   },
   function (error) {
-    //If there is an error, log it
+    // If there is an error, log it
     console.error(error);
   }
 );
 
-//Instantiate a new renderer and set its size
-const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+// Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); // Alpha: true allows for the transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.gammaOutput = true; // Enable gamma correction for brighter rendering
 
-//Add the renderer to the DOM
+// Add the renderer to the DOM
 document.getElementById("model").appendChild(renderer.domElement);
 
-//Set how far the camera will be from the 3D model
+// Set how far the camera will be from the 3D model
 camera.position.z =
   objToRender ===
   "https://cdn.glitch.global/8b3f5487-a392-4a06-ac03-d2334be70d1c/Anka_1.3(stor).glb?v=1707477561872"
-    ? 10
+    ? 11
     : 500;
 
-//Add lights to the scene, so we can actually see the 3D model
-const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x080820, 10); // (skyColor, groundColor, intensity)
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500); // top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, 1000);
+scene.add(ambientLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 10); // Adjust intensity here
 scene.add(hemisphereLight);
 
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 1); // (color, intensity)
-scene.add(ambientLight,
-  objToRender ===
-  "https://cdn.glitch.global/8b3f5487-a392-4a06-ac03-d2334be70d1c/Anka_1.3(stor).glb?v=1707477561872"
-    ? 5
-    : 1
-);
-scene.add(ambientLight);
-
-//Render the scene
+// Render the scene
 function animate() {
   requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
+  // Here we could add some code to update the scene, adding some automatic movement
 
-  //Make the eye move
+  // Make the eye move
   if (
     object &&
     objToRender ===
       "https://cdn.glitch.global/8b3f5487-a392-4a06-ac03-d2334be70d1c/Anka_1.3(stor).glb?v=1707477561872"
   ) {
-    //I've played with the constants here until it looked good
+    // I've played with the constants here until it looked good
     object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
     object.rotation.x = -1.2 + (mouseY * 2.5) / window.innerHeight;
   }
   renderer.render(scene, camera);
 }
 
-//Add a listener to the window, so we can resize the window and the camera
+// Add a listener to the window, so we can resize the window and the camera
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-//add mouse position listener, so we can make the eye move
+// Add mouse position listener, so we can make the eye move
 document.onmousemove = (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 };
 
-//Start the 3D rendering
+// Start the 3D rendering
 animate();
 
 
-//klick funktion 
+// Click function 
 var countdown = 10; // Initial countdown value
 
 // Function to update countdown display
